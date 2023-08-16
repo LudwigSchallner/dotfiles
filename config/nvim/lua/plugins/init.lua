0e4785a687c7bbc2d8208b6c104e9b224d80c137
Ludwig <29692215+LudwigSchallner@users.noreply.github.com>
2023-08-16 06:25:16 +0200

    style

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -4,38 +4,39 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
   "mechatroner/rainbow_csv",       --
-	"ervandew/supertab",            -- Tab completion in insert mode
-	"jiangmiao/auto-pairs",          -- Automatically close brackets
-	"junegunn/fzf",
-	"majutsushi/tagbar",             -- Module/class tag bar
-	"mgedmin/python-imports.vim",    -- Auto import for Python
-	"ellisonleao/gruvbox.nvim",
-	-- "psf/black",
-	"sheerun/vim-polyglot",         -- Color syntax for any language
-	"tmhedberg/SimpylFold",         -- Python folding
-	"tpope/vim-eunuch",             -- UNIX commands
-	"tpope/vim-fugitive",           -- Git support
-	"tpope/vim-repeat",             -- "." repeating maps from plugins
-	"tpope/vim-sensible",           -- Sensible defaults
-	"tpope/vim-surround",           -- Brackets and parenthesis and such
-	"tpope/vim-unimpaired",         -- See the help
-	"tveskag/nvim-blame-line",      -- Git blamer
-	"vim-airline/vim-airline",      -- Nice status line
-	"zhou13/vim-easyescape",        -- Map jk and kj to <ESC>
-	{ "sindrets/diffview.nvim",
+  "ervandew/supertab",            -- Tab completion in insert mode
+  "jiangmiao/auto-pairs",          -- Automatically close brackets
+  "junegunn/fzf",
+  "majutsushi/tagbar",             -- Module/class tag bar
+  "mgedmin/python-imports.vim",    -- Auto import for Python
+  "ellisonleao/gruvbox.nvim",
+  -- "psf/black",
+  -- "sheerun/vim-polyglot",         -- Color syntax for any language
+  "tmhedberg/SimpylFold",         -- Python folding
+  "tpope/vim-eunuch",             -- UNIX commands
+  "tpope/vim-fugitive",           -- Git support
+  "tpope/vim-repeat",             -- "." repeating maps from plugins
+  "tpope/vim-sensible",           -- Sensible defaults
+  "tpope/vim-surround",           -- Brackets and parenthesis and such
+  "tpope/vim-unimpaired",         -- See the help
+  "tveskag/nvim-blame-line",      -- Git blamer
+  "vim-airline/vim-airline",      -- Nice status line
+  "zhou13/vim-easyescape",        -- Map jk and kj to <ESC>
+  { "sindrets/diffview.nvim",
     config = {
       use_icons=false,
     }
   },
-	"zaldih/themery.nvim",
-	"wookayin/semshi",
+  "zaldih/themery.nvim",
+  "wookayin/semshi",
   "nvim-treesitter/nvim-treesitter",
+	"tree-sitter/tree-sitter-python",
   "williamboman/mason.nvim",
   "williamboman/mason-lspconfig.nvim",
   "neovim/nvim-lspconfig",
   "hrsh7th/nvim-cmp",
   "hrsh7th/cmp-nvim-lsp",
-    "L3MON4D3/LuaSnip",
+  "L3MON4D3/LuaSnip",
   "saadparwaiz1/cmp_luasnip",
   "rafamadriz/friendly-snippets",
   "mfussenegger/nvim-dap",
