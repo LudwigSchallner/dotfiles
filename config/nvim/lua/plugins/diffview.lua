def37b12c42d680f9e84568c5dba643d006572bd
Otterpatsch <29692215+LudwigSchallner@users.noreply.github.com>
2023-09-20 11:34:48 +0200

    add key map for diffview

--- config/nvim/lua/plugins/diffview.lua
+++ config/nvim/lua/plugins/diffview.lua
@@ -1,5 +1,8 @@
 return { "sindrets/diffview.nvim",
     config = function ()
+      local command = "git show-branch -a | grep '\\*' | grep -v `git rev-parse --abbrev-ref HEAD` | head -n1 | sed 's/.*\\[\\(.*\\)\\].*/\\1/' | sed 's/[\\^~].*//'"                                                                         
+      local base_branch = vim.fn.system(command)
+      vim.api.nvim_set_keymap("n", "<leader>dvb", ":DiffviewOpen " .. base_branch.."", { noremap = true})
       vim.api.nvim_set_keymap("n", "<leader>dv", ":DiffviewOpen ", { noremap = true})
       require("diffview").setup({
       	use_icons=false,
