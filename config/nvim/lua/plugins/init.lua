d543e6c8971537f09c6aeaf9050e0e71f7c9b310
Otterpatsch <29692215+LudwigSchallner@users.noreply.github.com>
2023-09-26 16:01:05 +0200

    enable mouse support in hardtime.nvim

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -3,7 +3,8 @@ return {
    "m4xshen/hardtime.nvim",
    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
    opts = {
-     restriction_mode="hint",
+    restriction_mode="hint",
+    disable_mouse=false,
    },
   },
   {
