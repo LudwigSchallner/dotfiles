b6d8a9029145cb53647672ef6bba39691220068e
Otterpatsch <29692215+LudwigSchallner@users.noreply.github.com>
2024-01-11 10:10:55 +0100

    feat: update treesitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -7,10 +7,13 @@ local M = {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = {"python", "c", "lua", },
+          ensure_installed = {"python", "c", "lua","yaml" },
           sync_install = false,
           highlight = { enable = true, additional_vim_regex_highlighting = true, },
-          indent = { enable = true },
+          indent = { 
+            enable = true,
+            disable = { 'yaml'},
+          },
 
         })
     end
