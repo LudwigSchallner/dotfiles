44a8055a4f524fb2fe7c8872ea863d88ee98101b
Otterpatsch <29692215+LudwigSchallner@users.noreply.github.com>
2024-02-14 12:01:03 +0100

    feat: add local bitbucket.nvim

---
+++ config/nvim/lua/plugins/bitbucket.lua
@@ -0,0 +1,9 @@
+return {
+  dir = "~/projects/bitbucket.nvim/",
+  deps = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "stevearc/dressing.nvim"
+  }
+}
