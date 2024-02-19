8a40a9161cc8132ea074597a053303387efed083
Otterpatsch <29692215+LudwigSchallner@users.noreply.github.com>
2024-02-19 21:41:04 +0100

    feat(bitbucket.nvim): update dependency

--- config/nvim/lua/plugins/bitbucket.lua
+++ config/nvim/lua/plugins/bitbucket.lua
@@ -1,9 +1,14 @@
 return {
-  dir = "~/projects/bitbucket.nvim/",
-  deps = {
-    "MunifTanjim/nui.nvim",
-    "nvim-lua/plenary.nvim",
-    "sindrets/diffview.nvim",
-    "stevearc/dressing.nvim"
-  }
+	dir = "~/projects/bitbucket.nvim/",
+	deps = {
+		"MunifTanjim/nui.nvim",
+		"nvim-lua/plenary.nvim",
+		"sindrets/diffview.nvim",
+		"stevearc/dressing.nvim",
+		"folke/noice.nvim",
+		"rcarriga/nvim-notify",
+	},
+	config = function()
+		require("bitbucket")
+	end,
 }
