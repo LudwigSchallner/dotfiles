2b18624e8e70a6483b9c43851a4923c6baf29cde
Otterpatsch <29692215+Otterpatsch@users.noreply.github.com>
2024-02-21 16:46:35 +0100

    feat(nvim-cmp): add sorting of completion entries

--- config/nvim/lua/plugins/nvim-cmp.lua
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,16 +1,45 @@
+local lspkind_comparator = function(conf)
+	local lsp_types = require("cmp.types").lsp
+	return function(entry1, entry2)
+		if entry1.source.name ~= "nvim_lsp" then
+			if entry2.source.name == "nvim_lsp" then
+				return false
+			else
+				return nil
+			end
+		end
+		local kind1 = lsp_types.CompletionItemKind[entry1:get_kind()]
+		local kind2 = lsp_types.CompletionItemKind[entry2:get_kind()]
+		if kind1 == "Variable" and entry1:get_completion_item().label:match("%w*=") then
+			kind1 = "Parameter"
+		end
+		if kind2 == "Variable" and entry2:get_completion_item().label:match("%w*=") then
+			kind2 = "Parameter"
+		end
+
+		local priority1 = conf.kind_priority[kind1] or 0
+		local priority2 = conf.kind_priority[kind2] or 0
+		if priority1 == priority2 then
+			return nil
+		end
+		return priority2 < priority1
+	end
+end
+
+local label_comparator = function(entry1, entry2)
+	return entry1.completion_item.label < entry2.completion_item.label
+end
 return {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp", -- use suggestions from the LSP
-
-		-- Snippet engine. Required for nvim-cmp to work, even if you don't
-		-- intend to use custom snippets.
 		"L3MON4D3/LuaSnip", -- snippet engine
 		"saadparwaiz1/cmp_luasnip", -- adapter for the snippet engine
 	},
 	config = function()
 		require("luasnip.loaders.from_vscode").lazy_load()
 		local cmp = require("cmp")
+		local compare = require("cmp.config.compare")
 		local luasnip = require("luasnip")
 		local select_opts = { behavior = cmp.SelectBehavior.Select }
 		cmp.setup({
@@ -85,13 +114,50 @@ return {
 				end, { "i", "s" }),
 			},
 			sources = cmp.config.sources({
-				{ name = "path" },
-				{ name = "nvim_lsp", keyword_length = 1 },
-				{ name = "buffer", keyword_length = 3 },
-				{ name = "luasnip", keyword_length = 2 },
-			}, {
+				{ name = "nvim_lsp" },
+				{ name = "nvim_lsp_signature" },
 				{ name = "buffer" },
+				{ name = "path" },
+				{ name = "luasnip" },
+			}, {
+				{ name = "path" },
+				{ name = "buffer", keyword_length = 5 },
 			}),
+			sorting = {
+				comparators = {
+					lspkind_comparator({
+						kind_priority = {
+							Parameter = 14,
+							Field = 11,
+							Property = 11,
+							Constant = 10,
+							Enum = 10,
+							EnumMember = 10,
+							Event = 10,
+							Function = 10,
+							Method = 10,
+							Operator = 10,
+							Reference = 10,
+							Struct = 10,
+							Variable = 9,
+							File = 8,
+							Folder = 8,
+							Class = 5,
+							Color = 5,
+							Module = 5,
+							Keyword = 2,
+							Constructor = 1,
+							Interface = 1,
+							Snippet = 0,
+							Text = 1,
+							TypeParameter = 1,
+							Unit = 1,
+							Value = 1,
+						},
+					}),
+					label_comparator,
+				},
+			},
 			window = {
 				documentation = cmp.config.window.bordered(),
 			},
