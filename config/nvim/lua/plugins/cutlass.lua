5924ea9258f52f3f2aa1c9d58a9d489f00d64d4d
Otterpatsch <29692215+Otterpatsch@users.noreply.github.com>
2024-09-16 11:31:40 +0200

    cleanup

--- config/nvim/lua/plugins/cutlass.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"svermeulen/vim-cutlass",
-	config = function()
-		vim.keymap.set({ "n", "v" }, "m", "d", { silent = true, noremap = true })
-		vim.keymap.set({ "n", "v" }, "mm", "dd", { silent = true, noremap = true })
-		vim.keymap.set({ "n", "v" }, "M", "D", { silent = true, noremap = true })
-	end,
-}
--- config/nvim/lua/plugins/highlight-undo.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"tzachar/highlight-undo.nvim",
-	config = function()
-		require("highlight-undo").setup({
-			hlgroup = "HighlightUndo",
-			duration = 300,
-		})
-	end,
-}
